9d77974dbb0a187f360b569e7b217eaac6d03f56
Jack Collins <44711059+0phoi5@users.noreply.github.com>
2025-02-12 11:27:56 +0000

    Create delete_vsphere_snapshots.ps1

---
+++ delete_vsphere_snapshots.ps1
@@ -0,0 +1,16 @@
+#!/usr/bin/pwsh
+
+$six_hours_ago = (Get-Date).AddHours(-6)
+$vs_servers = @("server1.domain.co.uk","server2.domain.co.uk")
+
+ForEach ($vs_server in $vs_servers) {
+  Write_host "Connecting to $vs_server"
+  Connect-VIServer $vs_server -User USERNAME -Password PASSWORD
+
+    Get-VM |
+    Get-Snapshot |
+    Where-Object Name -like *"YOUR SEARCH TERM HERE"* |
+    Remove-Snapshot -Confirm:$false
+
+  Disconnect-VIServer -Confirm:$false
+}
